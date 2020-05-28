680d8210ddc5e121b990e8d1f25a6630553f851f
Hugo Marques <hugo_filipe_marques@live.com.pt>
2020-05-28 10:03:36 +0100

    som

---
+++ MeioMundo.Editor.Ferramentas/Barcode/Barcode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeioMundoEditor.API.Plugin;
+
+namespace MeioMundo.Editor.Ferramentas.Barcode
+{
+    public class Barcode : IPlugin
+    {
+        public string Name => "Codigo de Barras";
+        public string Description => "Permite Gerar Codigos de Barras e de Data";
+        public Barcode()
+        {
+
+        }
+    }
+}
---
+++ MeioMundo.Editor.Ferramentas/Barcode/BarcodeInternal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace MeioMundo.Editor.Ferramentas.Barcode
+{
+    public class BarcodeInternal
+    {
+        public enum CodeType
+        {
+            Code39,
+            EAN13
+        }
+        public struct CODE
+        {
+            public string Code { get; set; }
+            public string Descrição { get; set; }
+            public CodeType CodeType { get; set; }
+        }
+
+        public BitmapImage GetImageCode(CODE code, Size size)
+        {
+            return null;
+        }
+    }
+}
